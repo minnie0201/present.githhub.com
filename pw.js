f652b0256b849681ea4a7a5e978cea607c97c863
minnie0201 <147067350+minnie0201@users.noreply.github.com>
2023-10-16 23:24:34 +0800

    Add files via upload

---
+++ pw.js
@@ -0,0 +1,12 @@
+<script>
+    function passcheck() {
+    function passcheck() {
+    var password=document.getElementById("pw").value;
+    var correctpassword="2023oct";
+    if (password===correctpassword) {
+        window.location.href = "next.html"};
+    else{
+        alert("正しくないパスワード")};
+}
+
+</script>
